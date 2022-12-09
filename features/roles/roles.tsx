258de5d7ab0012fb15493af47ae1224043af761e
Бобырев Дмитрий Михайлович <BobyrevDM@it2g.ru>
2022-12-09 21:35:56 +0500

    roles

--- features/roles/roles.tsx
+++ features/roles/roles.tsx
@@ -1,11 +1,12 @@
 import { FC, useState } from "react";
+import Image from "next/image";
 import RolesPeopleMentorImage from "@shared/assets/roles-peoples-mentor.png";
 import RolesPeopleReviewerImage from "@shared/assets/roles-peoples-reviewer.png";
+import RolesArrowImage from "@shared/assets/roles-arrow.svg";
 import { Typography } from "@shared/components";
 import { Tabs } from "@shared/components/tabs";
-import styles from "./roles.module.scss";
-import Image from "next/image";
 import { makeRolesTabsMap, Role } from "./utils";
+import styles from "./roles.module.scss";
 
 export const tabs = makeRolesTabsMap([Role.mentor, Role.reviewer]);
 
@@ -14,6 +15,9 @@ export const Roles: FC = () => {
 
   return (
     <div className={styles.roles}>
+      <div className={styles.roles__arrow}>
+        <RolesArrowImage />
+      </div>
       <div>
         {activeTab === Role.mentor && (
           <Image src={RolesPeopleMentorImage} alt="" />
