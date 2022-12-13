7d0cf7fba91a9e6b872c0daafc269764a619cd72
Бобырев Дмитрий Михайлович <BobyrevDM@it2g.ru>
2022-12-13 15:30:40 +0500

    Deploy

--- out/_next/static/ncAjibZ_eMc4RQpsBmlep/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/187-1e700958aa90bb4a.js","static/css/bbb26e28019067df.css","static/chunks/pages/index-a2ad4b9cd7407f36.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ out/_next/static/qP25iImpm_ng1_pXARLeD/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/187-b3c94165af962bd8.js","static/css/bbb26e28019067df.css","static/chunks/pages/index-a2ad4b9cd7407f36.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
