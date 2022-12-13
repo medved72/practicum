09faa6e0052ecd49090086d7685c5053b334da79
Бобырев Дмитрий Михайлович <BobyrevDM@it2g.ru>
2022-12-13 15:28:20 +0500

    Deploy

--- out/_next/static/tMbgLpBUvG3Bvy6WnHzfJ/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/187-7112a4a1d070d27a.js","static/css/bbb26e28019067df.css","static/chunks/pages/index-5304fbf277d2053b.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
