f98ef2da9b1269259eac1f725dac39fc9e2af5c7
Бобырев Дмитрий Михайлович <BobyrevDM@it2g.ru>
2022-12-18 18:31:27 +0500

    some for mobile

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,8 @@
 import ReactModal from "react-modal";
-import "../styles/globals.scss";
 import type { AppProps } from "next/app";
+import "../styles/globals.scss";
 
 export default function App({ Component, pageProps }: AppProps) {
   ReactModal.setAppElement("#__next");
-
   return <Component {...pageProps} />;
 }
